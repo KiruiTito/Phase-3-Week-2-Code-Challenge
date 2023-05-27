9822c34218f84913341c67548b5a4944063cbbe8
KiruiTito <titokirui402@gmail.com>
2023-05-27 21:17:30 +0300

    Completed assignment

---
+++ author.rb
@@ -0,0 +1,17 @@
+class Author
+    attr_reader :name
+  
+    @@all = []
+  
+    def initialize(name)
+      @name = name
+      @@all << self
+    end
+  
+    def self.all
+      @@all
+    end
+  
+    # Other methods for Author class
+  end
+  
