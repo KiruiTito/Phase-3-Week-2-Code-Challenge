0707f6ac3ae7098533cf64387b4d7ef3a4a13910
KiruiTito <titokirui402@gmail.com>
2023-05-29 10:03:55 +0300

    Completed

--- magazine/run.rb
+++ magazine/run.rb
@@ -1,11 +1,10 @@
-# Load the required files
+
 require_relative 'author'
 require_relative 'magazine'
 require_relative 'article'
 
-# Create instances of Author, Magazine, and Article classes
-author1 = Author.new("John Doe")
-author2 = Author.new("Jane Smith")
+author1 = Author.new("")
+author2 = Author.new("")
 
 magazine1 = Magazine.new("Fashion Magazine", "Fashion")
 magazine2 = Magazine.new("Tech Magazine", "Technology")
@@ -15,14 +14,9 @@ article2 = Article.new(author2, magazine1, "Beauty Tips")
 article3 = Article.new(author1, magazine2, "Latest Gadgets")
 article4 = Article.new(author2, magazine2, "Tech News")
 
-# Test the methods
 puts "Author Name: #{author1.name}"
 puts "Magazine Name: #{magazine1.name}"
 puts "Article Title: #{article1.title}"
 puts "Author of Article: #{article1.author.name}"
 puts "Magazine of Article: #{article1.magazine.name}"
 
-# More testing of methods
-# ...
-
-# Run the code in the terminal and check the output
